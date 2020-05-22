6d3bae5f2c19d30a82f8f9c849e4e301f7210ae4
Tingwei <tingwei9977@gmail.com>
2020-05-22 19:48:47 +1000

    first commit

---
+++ gnn.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction import image
+from PIL import Image
+import networkx as nx
+
+pic = Image.open('下载.jpeg').convert('RGB')
+data = np.array(pic).astype(np.float)
+graph = image.img_to_graph(data)
+
+print(type(graph))
+
+graph1 = nx.from_scipy_sparse_matrix(graph)
+
+G = nx.DiGraph()
+G.add_edges_from(
+    [('A', 'B'), ('A', 'C'), ('D', 'B'), ('E', 'C'), ('E', 'F'),
+     ('B', 'H'), ('B', 'G'), ('B', 'F'), ('C', 'G')])
+
+val_map = {'A': 1.0,
+           'D': 0.5714285714285714,
+           'H': 0.0}
+
+values = [val_map.get(node, 0.25) for node in G.nodes()]
+
+# Specify the edges you want here
+red_edges = [('A', 'C'), ('E', 'C')]
+edge_colours = ['black' if not edge in red_edges else 'red'
+                for edge in G.edges()]
+black_edges = [edge for edge in G.edges() if edge not in red_edges]
+
+# Need to create a layout when doing
+# separate calls to draw nodes and edges
+pos = nx.spring_layout(G)
+nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'),
+                       node_color=values, node_size=500)
+nx.draw_networkx_labels(G, pos)
+nx.draw_networkx_edges(G, pos, edgelist=red_edges, edge_color='r', arrows=True)
+nx.draw_networkx_edges(G, pos, edgelist=black_edges, arrows=False)
+plt.show()
